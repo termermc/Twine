925bad8d3925d70b8ac22a046c17f33acdfba095
= <termer@protonmail.com>
2019-11-18 18:14:02 -0500

    update start scripts

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 # Starts Twine with `dependencies/` and `modules/` included in the classpath.
 # Starting without the `--classpath-loaded` flag would create a new process with these options.
-java -classpath 'twine-1.0.jar:dependencies/*:modules/*' net.termer.twine.Twine -s --classpath-loaded
+java -classpath 'twine-1.1.jar:dependencies/*:modules/*' net.termer.twine.Twine -s --classpath-loaded
