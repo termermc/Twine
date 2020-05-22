f86a8e72e475bdae647b41b4728b6949db1b0413
= <termer@protonmail.com>
2020-05-22 14:30:05 -0400

    fix documentation generator script

--- mkdoc.sh
+++ mkdoc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-javadoc -d doc/ -sourcepath src/main/java/ -subpackages net.termer.twine net.termer.twine -classpath build/libs/twine-all.jar
+javadoc -d doc/ -sourcepath src/main/java/ -subpackages net.termer.twine net.termer.twine -classpath build/libs/twine-*-all.jar
